e48d743f0d40f6d7d41112aa9cec5d220227bfc2
Isaac Halvorson <hello@hisaac.net>
2016-11-28 13:34:34 -0600

    build client side routes

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,1 +1,26 @@
+//----------------------------------------------------------------------------//
+var app = angular.module('desk', ['ngRoute']); // instantiates app
+//----------------------------------------------------------------------------//
 
+//------------------------------- ROUTE CONFIG -------------------------------//
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/newhero', {
+      templateUrl: 'views/tempates/newhero.html',
+      controller: 'HeroController',
+      controllerAs: 'heroctrl'
+    })
+    .when('/herodb', {
+      templateUrl: 'views/templates/herodb.html',
+      controller: 'HeroDb',
+      controllerAs: 'herodb'
+    })
+    .otherwise({ redirectTo: 'herodb' });
+}]);
+//----------------------------------------------------------------------------//
+
+//--------------------------------CONTROLLERS---------------------------------//
+
+
+
+//----------------------------------------------------------------------------//
